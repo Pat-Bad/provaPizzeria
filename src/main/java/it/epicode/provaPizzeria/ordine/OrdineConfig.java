b5c1120170c5c506a82362890da0a6b6099c9986
Patricia <patricia.badji89@gmail.com>
2025-02-04 16:58:59 +0100

    new ex.

--- src/main/java/it/epicode/provaPizzeria/ordine/OrdineConfig.java
+++ src/main/java/it/epicode/provaPizzeria/ordine/OrdineConfig.java
@@ -1,19 +1,24 @@
 package it.epicode.provaPizzeria.ordine;
 
+import it.epicode.provaPizzeria.calcoloPrezzi.elementoMenu;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class OrdineConfig {
     @Bean
     public Ordine ordine () {
+        List<elementoMenu> elementiOrdinati = new ArrayList<>();
+
         Ordine ordine1 = new Ordine();
         ordine1.setNumeroOrdine(1);
         ordine1.setStatoOrdine("In corso");
         ordine1.setNumeroCoperti(2);
         ordine1.setOraAcquisizione(Time.valueOf("14:42:00"));
-        ordine1.setElementiOrdinati(pizzaFunghi);
+
 
         return ordine1;}}
--- src/main/java/it/epicode/provaPizzeria/runner/applicationRunner.java
+++ src/main/java/it/epicode/provaPizzeria/runner/applicationRunner.java
@@ -9,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Component
 @RequiredArgsConstructor
@@ -18,10 +21,14 @@ public class applicationRunner implements CommandLineRunner {
     private final Ordine ordine;
     private final Tavolo tavolo1;
     private final Ordine ordine1;
+   private final List<elementoMenu> elementiOrdinati = new ArrayList<>();
 
     @Override
     public void run(String... args) throws Exception {
         //System.out.println(menu);
+        elementiOrdinati.add(menuNuovo.getPizza());
+        elementiOrdinati.add(menuNuovo.getBevanda());
+        ordine1.setElementiOrdinati(elementiOrdinati);
        tavolo1.setOrdine(ordine1);
         System.out.println(ordine1);
 
